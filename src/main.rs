5d2f2e319fcf9c979c95a3abe224e7c37b854384
bigbass1997 <bigbass1997.website@gmail.com>
2021-09-08 00:53:31 -0500

    Added ROM header parsing

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ extern crate strum_macros;
 mod rom;
 mod disassembly;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use crate::disassembly::Disassembly;
 use std::fs::{File, FileType};
 use std::io::Write;
+use crate::rom::{Header, Rom};
 
 
 fn main() {
     //save_disassembly(disassemble_ipl3_headerless("/data/storage/roms/n64-nointro/Star Fox 64 (USA).z64"), "/data/storage/roms/n64-nointro/Star Fox 64 (USA).z64.IPL3.disasm");
     //save_disassembly(disassemble_ipl3_headerless("/data/storage/roms/n64-nointro/Lylat Wars (Europe) (En,Fr,De).z64"), "/data/storage/roms/n64-nointro/Lylat Wars (Europe) (En,Fr,De).z64.IPL3.disasm");
     //save_disassembly(disassemble_pifrom("/data/storage/preservation/pifdata.bin"), "/data/storage/preservation/pifdata.bin.disasm");
-    save_disassembly(disassemble_ipl3_headerless("/data/storage/roms/n64-nointro/Conker's Bad Fur Day (USA).z64"), "/data/storage/roms/n64-nointro/Conker's Bad Fur Day (USA).z64.IPL3.disasm");
+    //save_disassembly(disassemble_ipl3_headerless("/data/storage/roms/n64-nointro/Conker's Bad Fur Day (USA).z64"), "/data/storage/roms/n64-nointro/Conker's Bad Fur Day (USA).z64.IPL3.disasm");
+    save_disassembly(disassemble_pifrom("/data/storage/roms/n64-nointro/Namco Museum 64 (USA).z64"), "/data/storage/roms/n64-nointro/Namco Museum 64 (USA).z64.disasm");
     
     /*let mut count = 0;
     let mut disasms = Vec::new();
@@ -63,6 +65,117 @@ fn main() {
             }
         }
     }*/
+    
+    //dump_headers("/data/storage/roms/n64-nointro");
+}
+
+fn dump_headers(path_str: &str) {
+    let path = Path::new(path_str);
+    
+    let mut valid_counter = 0;
+    let mut header_counter = 0;
+    let mut save_counter = 0;
+    
+    let mut out_path = String::new();
+    let mut valid_paths: Vec<PathBuf> = Vec::new();
+    let mut headers: Vec<Header> = Vec::new();
+    if path.is_dir() {
+        let dir = path.read_dir().unwrap();
+        
+        for wrapped_entry in dir {
+            let entry = wrapped_entry.unwrap();
+            let path = entry.path();
+            let extension = path.extension();
+            
+            if !path.is_dir() && extension.is_some() && extension.unwrap().eq("z64") {
+                valid_paths.push(path);
+                valid_counter += 1;
+            }
+        }
+        
+        out_path.push_str(&[path_str, "/output.csv"].concat());
+    } else {
+        valid_paths.push(path.to_path_buf());
+        
+        out_path.push_str(&[path_str, ".output.csv"].concat());
+        
+        valid_counter += 1;
+    }
+    
+    println!("Valid Paths: {}", valid_counter);
+    
+    for path in &valid_paths {
+        let read_result = std::fs::read(path);
+        if read_result.is_ok() {
+            let rom = Rom::new(read_result.unwrap());
+            
+            headers.push(rom.header);
+            
+            header_counter += 1;
+            println!("Headers Extracted: {} of {}", header_counter, valid_counter);
+        }
+    }
+    
+    fn u8arr_str(val: &[u8]) -> String { 
+        let result = String::from_utf8(val.to_vec());
+        
+        if result.is_ok() {
+            return result.unwrap();
+        }
+        
+        let mut out = String::from("0x");
+        for b in val {
+            out.push_str(&format!("{:02X}", b));
+        }
+        
+        out
+    }
+    fn u32_str(val: u32) -> String {
+        let result = String::from_utf8(val.to_be_bytes().to_vec());
+        
+        if result.is_ok() {
+            return result.unwrap();
+        }
+        
+        format!("{:#010X}", val)
+    }
+    fn u16_str(val: u16) -> String {
+        let result = String::from_utf8(val.to_be_bytes().to_vec());
+        
+        if result.is_ok() {
+            return result.unwrap();
+        }
+        
+        format!("{:#06X}", val)
+    }
+    
+    let mut csv = String::from("file_name;pi_regs;clockrate;pc;release;crc1;crc2;unknown0;image_name;unknown1;manufacturer_id;cart_id;country\n");
+    for header in headers {
+        let file_name = valid_paths[save_counter].file_name().unwrap().to_os_string();
+        
+        csv.push_str(&format!("{};{:#010X};{:#010X};{:#010X};{:#010X};{:#010X};{:#010X};{:#018X};{};{:#010X};{};{};{}\n",
+            file_name.to_str().unwrap(),
+            header.pi_regs,
+            header.clockrate,
+            header.pc,
+            header.release,
+            header.crc1,
+            header.crc2,
+            header.unknown0,
+            u8arr_str(&header.image_name),
+            header.unknown1,
+            u32_str(header.manu_id),
+            u16_str(header.cart_id),
+            u16_str(header.country),
+        ));
+        
+        save_counter += 1;
+        println!("Headers Parsed: {} of {}", save_counter, valid_counter);
+    }
+    
+    let mut out = File::create(out_path).unwrap();
+    out.write_all(csv.as_bytes()).unwrap();
+    println!("Complete!");
 }
 
 fn disassemble_ipl3_headerless(path: &str) -> Disassembly {
@@ -86,6 +199,6 @@ fn disassemble_pifrom(path: &str) -> Disassembly {
 fn save_disassembly(disasm: Disassembly, path: &str) {
     let mut out = File::create(path).unwrap();
     for (i, instr) in disasm.instructions.iter().enumerate() {
-        out.write_all(format!("[{:#010X}]{}\n", (i * 4) + 0x40, instr).as_bytes()).unwrap();
+        out.write_all(format!("[{:#010X}]{}\n", (i * 4) + 0x00, instr).as_bytes()).unwrap();
     }
 }
